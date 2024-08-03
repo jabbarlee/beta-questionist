ce0d408867d5cc6c47bbc6d1ead4e43dd024a714
jabbarlee <amiljabbarlee@gmail.com>
2024-08-02 21:26:00 -0500

    buttons placed in configSide

--- src/components/ConfigSide/index.tsx
+++ src/components/ConfigSide/index.tsx
@@ -5,7 +5,7 @@ import AccordionSelection from '../Accordion';
 import styles from './index.module.css';
 import { handleChipClick } from '@/actions/handleChipClick';
 import { accordions, units } from '@/lib/data/questionConfig';
-import { Typography, Divider } from '@mui/material';
+import { Typography, Divider, Button } from '@mui/material';
 
 const ConfigSide = () => {
   const [selectedChipsState, setSelectedChipsState] = useState<{ [key: string]: string[] }>({});
@@ -24,30 +24,37 @@ const ConfigSide = () => {
   
   return (
     <div className={styles.configSide}>
-      <Typography variant="h5">Configure the question</Typography>
-      <br />
-      <Divider />
-      <br />
-      {accordions.map((accordion) => (
-        <AccordionSelection
-          key={accordion.id}
-          title={accordion.title}
-          metaText={accordion.metaText}
-          chips={accordion.chips}
-          accordionId={accordion.id}
-          selectedChips={selectedChipsState[accordion.id] || []}
-          onChipClick={(chip) => handleChipClick({ accordionId: accordion.id, chip, setSelectedChipsState })}
-        />
-      ))}
-
-      <AccordionSelection
-        title="Select Units"
-        metaText="Select the units based on your previous selections."
-        chips={filteredUnits}
-        accordionId="unitsAccordion"
-        selectedChips={selectedChipsState['unitsAccordion'] || []}
-        onChipClick={(chip) => handleChipClick({ accordionId: 'unitsAccordion', chip, setSelectedChipsState })}
-      />
+      <div className={styles.mainContainer}>
+        <div className={styles.accordionsContainer}>
+          <Typography variant="h5">Configure the question</Typography>
+          <br />
+          <Divider />
+          <br />
+          {accordions.map((accordion) => (
+            <AccordionSelection
+              key={accordion.id}
+              title={accordion.title}
+              metaText={accordion.metaText}
+              chips={accordion.chips}
+              accordionId={accordion.id}
+              selectedChips={selectedChipsState[accordion.id] || []}
+              onChipClick={(chip) => handleChipClick({ accordionId: accordion.id, chip, setSelectedChipsState })}
+            />
+          ))}
+          <AccordionSelection
+            title="Select Units"
+            metaText="Select the units based on your previous selections."
+            chips={filteredUnits}
+            accordionId="unitsAccordion"
+            selectedChips={selectedChipsState['unitsAccordion'] || []}
+            onChipClick={(chip) => handleChipClick({ accordionId: 'unitsAccordion', chip, setSelectedChipsState })}
+          />
+        </div>
+        <div className={styles.buttonContainer}>
+          <Button variant='outlined'>Reset</Button>
+          <Button variant="contained">Generate</Button>
+        </div>
+      </div>
     </div>
   );
 };
